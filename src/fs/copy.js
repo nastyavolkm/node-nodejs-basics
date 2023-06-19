25bfac5438c4c6069cca88055640c62c44fa84d8
Anastasiya_Mianko <anastasiya_mianko@epam.com>
2023-06-19 22:27:09 +0300

    feat: add copy task

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,21 @@
+import { cpSync, existsSync } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const copy = async () => {
-    // Write your code here 
+    const sourceFolderPath = path.join(__dirname, 'files');
+    const destinationFolderPath = path.join(__dirname, 'files_copy');
+    if (!existsSync(sourceFolderPath) || existsSync(destinationFolderPath)) {
+        throw new Error('FS operation failed');
+    } else {
+        cpSync(sourceFolderPath, destinationFolderPath, {
+            recursive: true,
+          });    
+    }
 };
 
 await copy();
