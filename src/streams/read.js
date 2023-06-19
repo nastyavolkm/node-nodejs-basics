854c34be30c3bd43c6d94d0b00f8da3c07806289
Anastasiya_Mianko <anastasiya_mianko@epam.com>
2023-06-20 01:14:48 +0300

    feat: add read Stream task

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,16 @@
+import path from 'path';
+import { createReadStream } from 'fs';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 const read = async () => {
-    // Write your code here 
+    const filePath = path .join(__dirname, 'files/fileToRead.txt');
+    const readStream = createReadStream(filePath);
+    readStream.on('data', data => {
+        process.stdout.write(data);
+    });
 };
 
 await read();
