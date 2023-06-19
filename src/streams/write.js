b2610d018df22dfa462d5829a0c3aa95553ee7cb
Anastasiya_Mianko <anastasiya_mianko@epam.com>
2023-06-20 01:27:01 +0300

    feat: add write stream task

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,18 @@
+import path from 'path';
+import { createWriteStream } from 'fs';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const write = async () => {
-    // Write your code here 
+    const filePath = path.join(__dirname, 'files/fileToWrite.txt');
+    const writableStream = createWriteStream(filePath);
+
+    process.stdin.pipe(writableStream);
+
+    process.stdin.resume();
 };
 
 await write();
