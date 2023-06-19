937c6e232cfbdfb3b758d895c870a93b000850d5
Anastasiya_Mianko <anastasiya_mianko@epam.com>
2023-06-19 23:07:07 +0300

    feat: add read task

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,19 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+    const fileToRead = path.join(__dirname, 'files/fileToRead.txt');
+    fs.readFile(fileToRead, 'utf8', (error, data) => {
+        if (error) {
+            throw new Error('FS operation failed');
+        }
+        console.log(data);
+    })
 };
 
 await read();
