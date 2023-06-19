0240eb3017120a6a07e3a5e532ef2672224f9926
Anastasiya_Mianko <anastasiya_mianko@epam.com>
2023-06-20 01:05:39 +0300

    feat: add hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,22 @@
+import { createHash } from "crypto";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import { readFile } from 'fs';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const calculateHash = async () => {
-    // Write your code here 
+    const filePath = path.join(__dirname, 'files/fileToCalculateHashFor.txt');
+    readFile(filePath, (error, data) => {
+        if (error) {
+            throw new error('Something went wrong');
+        } else {
+            const hash = createHash("sha256").update(data).digest("hex");
+            console.log(hash);
+        }
+    });
 };
 
 await calculateHash();
