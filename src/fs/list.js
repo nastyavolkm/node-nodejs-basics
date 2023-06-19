4ca38942926970a1a9ffaee0ecd8a9c2098406b5
Anastasiya_Mianko <anastasiya_mianko@epam.com>
2023-06-19 23:02:21 +0300

    feat: add list task

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,23 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const list = async () => {
-    // Write your code here 
+    const folderPath = path.join(__dirname, 'files');
+    fs.readdir(folderPath, (error, files) => {
+        if (error) {
+            errorHandler();
+        }
+        console.log(files);
+    })
 };
 
 await list();
+
+const errorHandler = () => {
+    throw new Error('FS operation failed');
+}
