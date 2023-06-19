ab769c08a40f82d1a772a5324f0e1a6a562d4efc
Anastasiya_Mianko <anastasiya_mianko@epam.com>
2023-06-19 22:45:16 +0300

    feat: add rename task

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,27 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const rename = async () => {
-    // Write your code here 
-};
+    const sourceFilePath = path.join(__dirname, 'files/wrongFilename.txt');
+    const resultFilePath = path.join(__dirname, 'files/properFilename.md');
+    if (fs.existsSync(resultFilePath) || !fs.existsSync(sourceFilePath)) {
+        errorHandler();
+    } else {
+        fs.rename(sourceFilePath, resultFilePath, (error) => {
+            if (error) {
+                errorHandler();
+            }
+        }); 
+    }
+}
+
+const errorHandler = () => {
+    throw new Error('FS operation failed');
+}
 
 await rename();
