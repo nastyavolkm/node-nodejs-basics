60355084f434ae95f1abdc719fa714851c5a79db
Anastasiya_Mianko <anastasiya_mianko@epam.com>
2023-06-19 23:42:33 +0300

    feat: add env task

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,15 @@
 const parseEnv = () => {
-    // Write your code here 
+    const variables = process.env;
+    const RSSVariables = Object.entries(variables)
+    .filter(([key]) => {
+        return key.startsWith('RSS_')
+        })
+        .map(([key, value]) => {
+            return `${key}=${value}`;
+        })
+        .join(';');
+
+    console.log(RSSVariables);
 };
 
 parseEnv();
